e2d0d6a9a8bb4cbc09f56d8e2a19e0b44944e9a9
paulos-f <souzafontanelapaulo@gmail.com>
2023-06-09 16:12:10 -0300

    1.4

--- fe.py
+++
@@ -1,5 +0,0 @@
-indentificador_do_produto = 0
-
-while indentificador_do_produto != 5:
-    indentificador_do_produto += 1
-    print(indentificador_do_produto)
--- nome/padaria.py
+++ nome/padaria.py
@@ -1,7 +1,7 @@
 
 print('\n1 - Cadastrar Produto.')
-print('2 - Alterar Produto.')
-print('3 - Realizar Venda.')
+print('2 - Realizar Venda.')
+print('3 - Alterar Produto.')
 print('4 - Relatórios.')
 print('5 – Sair.')
 
@@ -13,6 +13,8 @@ indentificador_do_produto = 0
 dict_nome_produtos = {"pão d'agua" : ["pão", 0.99, 100]}
 
 dict_codigo_produtos = {}
+
+dict_registro_vendas = {}
 
 while(True):
 
@@ -36,6 +38,8 @@ while(True):
             preço_produto = float(input('Qual o preço do produto? '))
             estoque_produto = int(input('Qual o estoque incial do produto? '))
 
+            # Sobre o indentificador de produtos, não conseguimos integrar ele no codigo da maneira correta.
+
             
             lista_tipo_preço_estoque.append(tipo_produto)
             lista_tipo_preço_estoque.append(preço_produto)
@@ -52,31 +56,40 @@ while(True):
             elif repitir == 'N':
                 break
         break
+
+
+
+
     elif resposta == 2:
+
         nome_venda = input('Qual produto você deseja realizar a venda?(Nome) ')
-        z = dict_nome_produtos.get(nome_venda)
-        print(z)
+        confirmação_estoque = dict_nome_produtos.get(nome_venda)
+        quant_estoque = confirmação_estoque[2]
+        quant_venda = int(input('Quantos {} você deseja vender? '.format(nome_venda))) 
+        
 
+        
+        if quant_venda > quant_estoque:
+            print('Você tem {} {} em estoque'.format(quant_estoque, nome_venda))
+            print('Não há estoque suficiente para esse produto...')
+        
+        
+        else:
+            dict_registro_vendas.update({nome_venda : quant_venda})
+            quant_estoque = quant_estoque - quant_venda
+            dict_nome_produtos.update({nome_venda : [confirmação_estoque[0], confirmação_estoque[1], quant_estoque]})
+        print('Sobrou {} no estoque'.format(quant_estoque))
+        print(dict_registro_vendas)   
+        # o problema atual é que o registro de vendas está atualizando, porém, ele apaga os dados das ultimas transações
 
+  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        repitir2 = input('Deseja vender outro produto?(S/N) ')
+        if repitir2 == 'S':
+            pass
+        elif repitir2 == 'N':
+            break
+        
 
 
 print(dict_nome_produtos)
