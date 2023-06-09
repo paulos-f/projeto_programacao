4902933fab370338c942b5d94259edba8d18d83e
paulos-f <souzafontanelapaulo@gmail.com>
2023-06-09 14:32:20 -0300

    1.3

---
+++ fe.py
@@ -0,0 +1,5 @@
+indentificador_do_produto = 0
+
+while indentificador_do_produto != 5:
+    indentificador_do_produto += 1
+    print(indentificador_do_produto)
--- nome/padaria.py
+++ nome/padaria.py
@@ -8,36 +8,76 @@ print('5 – Sair.')
 
 resposta = int(input('\nOlá, o que você deseja fazer? '))
 
+indentificador_do_produto = 0
 
-dict = {}
+dict_nome_produtos = {"pão d'agua" : ["pão", 0.99, 100]}
+
+dict_codigo_produtos = {}
 
 while(True):
+
+
     if resposta == 5:
         print('Te vejo em breve!!!')
         break
+
+
     elif resposta == 1:
+
+
         while(True):
-            print('Muito bem, vamos começar o cadastro. ')
+            print('\nMuito bem, vamos começar o cadastro. ')
 
             lista_tipo_preço_estoque = []
             
-            indentificador_do_produto = int(input('Qual o código você deseja colocar no produto? '))
+            indentificador_do_produto = input('Qual o código você deseja colocar no produto? ')
             nome_produto = input('Qual o nome do produto? ')
             tipo_produto = input('Qual o tipo do produto? ')
             preço_produto = float(input('Qual o preço do produto? '))
             estoque_produto = int(input('Qual o estoque incial do produto? '))
 
+            
             lista_tipo_preço_estoque.append(tipo_produto)
             lista_tipo_preço_estoque.append(preço_produto)
             lista_tipo_preço_estoque.append(estoque_produto)
             
-            dict.update({nome_produto : lista_tipo_preço_estoque})
+            dict_nome_produtos.update({nome_produto : lista_tipo_preço_estoque})
+
+            dict_codigo_produtos.update({indentificador_do_produto : nome_produto})
+
+
             repitir = input('Deseja adicionar outro produto?(S/N) ')
             if repitir == 'S':
                 pass
             elif repitir == 'N':
                 break
         break
+    elif resposta == 2:
+        nome_venda = input('Qual produto você deseja realizar a venda?(Nome) ')
+        z = dict_nome_produtos.get(nome_venda)
+        print(z)
 
 
-print(dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(dict_nome_produtos)
+print(dict_codigo_produtos)
